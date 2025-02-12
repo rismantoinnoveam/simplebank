ead2596e2008598fb6da788b93e2eebe064bcc3a
rismanto <rismanto@RismantoPC.>
2025-02-12 16:35:31 +0700

    add environment

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -7,11 +7,7 @@ import (
 	"testing"
 
 	_ "github.com/lib/pq"
-)
-
-const (
-	dbDriver = "postgres"
-	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	"github.com/techschool/simplebank/db/util"
 )
 
 var testQueries *Queries
@@ -19,7 +15,12 @@ var testDB *sql.DB
 
 func TestMain(m *testing.M) {
 	var err error
-	conn, err := sql.Open(dbDriver, dbSource)
+
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
